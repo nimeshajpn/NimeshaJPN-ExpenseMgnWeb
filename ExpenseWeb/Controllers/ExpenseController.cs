04f94c478ca536c7c0452d89213159045ab48a25
Nadeera Nimesha <Nadeeranimesha456@gmail.com>
2022-07-28 12:57:08 +0530

    done

--- ExpenseWeb/Controllers/ExpenseController.cs
+++ ExpenseWeb/Controllers/ExpenseController.cs
@@ -1,5 +1,6 @@
 ﻿using ExpenseWeb.DbConnection;
 using ExpenseWeb.Models;
+using ExpenseWeb.WebApi;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseWeb.Controllers
@@ -7,26 +8,40 @@ namespace ExpenseWeb.Controllers
 
     
 
-    public class ExpenseController : Controller
+    public class ExpenseController : Controller 
     {
       public  double I;
         double E;
         double A;
         double B;
+        IEnumerable<MExpense> Get;
+
 
         private readonly EDbContext _db;
+        
 
         public ExpenseController(EDbContext db)
         {
             _db = db;
+      
         }
 
         public IActionResult Index(Mexpense2 e,double tb)
         {
 
-            IEnumerable<MExpense> obj = _db.TbExpensies;
+           // IEnumerable<MExpense> obj = _db.TbExpensies;
+            IEnumerable<MExpense> obj = null;
 
-            Total(obj);
+
+
+
+
+            //obj2 = _db.TbExpensies;
+
+
+            IEnumerable<MExpense> obj2= Api(obj);
+            Total(obj2);
+            
             e.expanse = E;
             e.income = I;
             e.Total = I - E;
@@ -34,16 +49,20 @@ namespace ExpenseWeb.Controllers
             ViewBag.Total = e.Total;
             ViewBag.income = e.income;
             ViewBag.expence = e.expanse;
-            return View(obj);
+            return View(obj2);
 
         }
 
         public IActionResult All(Mexpense2 e, double tb)
         {
 
-            IEnumerable<MExpense> obj = _db.TbExpensies;
+           // IEnumerable<MExpense> obj = _db.TbExpensies; 
+            IEnumerable<MExpense> obj = null;
 
-            Total1(obj);
+
+
+            IEnumerable<MExpense> obj2 = Api(obj);
+            Total1(obj2);
             e.expanse = E;
             e.income = I;
             e.Total = I - E;
@@ -51,7 +70,7 @@ namespace ExpenseWeb.Controllers
             ViewBag.Total = e.Total;
             ViewBag.income = e.income;
             ViewBag.expence = e.expanse;
-            return View(obj);
+            return View(obj2);
 
         }
 
@@ -70,12 +89,16 @@ namespace ExpenseWeb.Controllers
         [HttpPost]
         public IActionResult Create(MExpense e) {
 
+            int id = 0;
+            int i = 2;
+            Api2(e, i,id);
+            
             if (e.Amount != 0 )
             {
 
 
-                _db.TbExpensies.Add(e);
-                _db.SaveChanges();
+             //   _db.TbExpensies.Add(e);
+             //   _db.SaveChanges();
 
             }
           
@@ -90,7 +113,12 @@ namespace ExpenseWeb.Controllers
 
         public IActionResult Edit(int? id)
         {
-            var e = _db.TbExpensies.Find(id);
+            MExpense e = null;
+
+            //  var e = _db.TbExpensies.Find(id);
+            int i = 3;
+           
+            e = Api2(e, i, id);
             return View(e);
 
         }
@@ -99,29 +127,41 @@ namespace ExpenseWeb.Controllers
         [HttpPost]
         public IActionResult Edit(MExpense e)
         {
-
+            int id = 0;
+            int i = 4;
+            Api2(e, i, id);
             if (e.Amount != 0)
             {
                
                
 
 
-                _db.TbExpensies.Update(e);
-                _db.SaveChanges();
+     //           _db.TbExpensies.Update(e);
+         //       _db.SaveChanges();
 
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("All");
 
         }
         public IActionResult Delete(int? id)
         {
-            var e = _db.TbExpensies.Find(id);
 
-            _db.TbExpensies.Remove(e);
-            _db.SaveChanges();
+            MExpense e = null;
+            int i = 5;
+            Api2(e, i, id);
 
-        
+
+
+
+
+
+            //    var e = _db.TbExpensies.Find(id);
+
+            //    _db.TbExpensies.Remove(e);
+            //     _db.SaveChanges();
+
+
 
             return RedirectToAction("All");
         }
@@ -192,7 +232,101 @@ namespace ExpenseWeb.Controllers
 
         }
 
+        public IEnumerable<MExpense> Api(IEnumerable<MExpense> obj) {
 
+
+
+
+
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://localhost:7178/");
+                
+
+
+                
+                    var r = client.GetAsync("Expense");
+                    r.Wait();
+
+                    var result = r.Result;
+
+                    var readjob = result.Content.ReadAsAsync<IList<MExpense>>();
+                    readjob.Wait();
+                    obj = readjob.Result;
+
+
+
+               
+
+            }
+        
+        return obj;
+        }
+        
+        public MExpense Api2(MExpense e, int i,int? id) {
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://localhost:7178/");
+
+
+
+                if (i == 2)
+                {
+                    var r = client.PostAsJsonAsync<MExpense>("Expense", e);
+                    r.Wait();
+
+                    var result = r.Result;
+
+
+                }
+                else if (i == 3)
+                {
+
+
+
+                    var r = client.GetAsync("Expense/" + id.ToString());
+                    r.Wait();
+
+                    var result = r.Result;
+
+                    var readjob = result.Content.ReadAsAsync<MExpense>();
+                    readjob.Wait();
+                    e = readjob.Result;
+
+                }
+                else if (i == 4)
+                {
+
+                    var r = client.PutAsJsonAsync<MExpense>("Expense/" + e.Id, e);
+                    r.Wait();
+
+                    var result = r.Result;
+
+
+
+                }
+                else if (i == 5) {
+
+
+
+                    var r = client.DeleteAsync("Expense/" + id.ToString());
+                    r.Wait();
+
+                    var result = r.Result;
+
+                    var readjob = result.Content.ReadAsAsync<MExpense>();
+                    readjob.Wait();
+
+                }
+
+            }
+
+
+
+            return e;
+        }
 
 
 
---
+++ ExpenseWeb/WebApi/ApiConnection.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpenseWeb.WebApi
+{
+    public class ApiConnection
+    {
+        static async Task Main(string[] args)
+        {
+
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+
+                var Get = await client.GetAsync("https://localhost:44358/Expense/");
+                
+            }
+
+              
+        }        
+
+
+    }
+}
